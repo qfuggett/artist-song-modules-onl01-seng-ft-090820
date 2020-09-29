e7f57cdf41c691e5ead82229e3c9c8a38ba2e109
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-29 16:14:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods      #defines the method as a class method, vs instance method (uses include keyword)
   extend Findable::ClassMethods
+  include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
@@ -15,9 +16,6 @@ class Artist
     @songs = []
   end
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
 
   def self.all
     @@artists
@@ -32,10 +30,5 @@ class Artist
     songs.each { |song| add_song(song) }
   end
   
-
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
   
 end
