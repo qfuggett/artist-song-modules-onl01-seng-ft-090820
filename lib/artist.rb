d1177586ce669389887c8f528836f4cd34365a06
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-29 16:02:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,15 @@
+# require_relative '../config/environment'
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
+    super
     @@artists << self
     @songs = []
   end
@@ -19,14 +22,6 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -35,8 +30,11 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
+  
+
 
   def to_param
     name.downcase.gsub(' ', '-')
   end
+  
 end
