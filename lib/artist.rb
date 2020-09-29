3a4d2a8a1c7eda9814b7155648867072691d4da0
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-29 16:20:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,6 +5,8 @@ class Artist
   extend Memorable::ClassMethods      #defines the method as a class method, vs instance method (uses include keyword)
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :songs
 
